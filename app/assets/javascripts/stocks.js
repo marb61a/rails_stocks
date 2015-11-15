7a40e4f2ed47e10df32db11c219792e01c56f8b0
marb <marb61a@gmail.com>
2015-11-15 15:03:29 +0000

    added spinner

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -7,10 +7,19 @@ init_stock_lookup = function(){
     });
     
      $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+         hide_spinner();
          $('#stock-lookup-results').replaceWith('');
          $('#stock-lookup-errors').replaceWith('Stock Was Not Found');
      })
 };
+
+$('#stock-lookup-form').on('ajax:before', function(event, data, status){
+    show_spinner();
+});
+
+$('#stock-lookup-form').on('ajax:after', function(event, data, status){
+    hide_spinner();
+});
 
 $(document).ready(function(){
     init_stock_lookup();
